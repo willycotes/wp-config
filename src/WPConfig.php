d105f006de25d3f39d3f3b2b456b7fdc25e5da81
Willy Cotes <willycotes@gmail.com>
2022-04-25 11:38:43 -0500

    first commit

---
+++ src/WPConfig.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Class Configuration with dotenv support
+ */
+
+namespace WPConfig;
+
+if ( ! class_exists( 'WPConfig' ) ) {
+
+	/**
+	 * Custom Class Configuration Environment WordPress using DotEnv dependency
+	 */
+  class WPConfig {
+
+			/**
+		 * Defined constant configuration
+		 * 
+		 * Lanza una exception si la constante ya está definida y si no devuelve false.
+		 */
+		protected static function defined( string $key ) {
+			if ( defined( $key ) ) {
+				$message = "Aborted trying to redefine constant '$key'. `define('$key', ...)` has already been occurred elsewhere.";
+				throw new \Exception( $message );
+			}
+			return false;
+		}
+
+		/**
+		 * Define constant configuration
+		 * 
+		 * Verifica los archivos de configuración y revisa si hay una constante que ya este definida, captura y lanza una excepción solo de aquellas constantes que se intentan redefinir con un valor diferente y al final solo define las constantes que no están definidas. De esta forma podemos corregir aquellas constantes que se intentan redefinir con un valor diferente.
+		 * 
+		 */
+		public static function define( string $key, $value ) {
+			/* Captura y lanza una excepción de aquellas constantes que ya existen con un valor diferente para evitar redefinirla. */
+			try {
+				self::defined( $key );
+			} catch( Exception $e ) {
+				if ( constant( $key ) !== $value ) {
+					throw $e;
+				}
+			}
+			/* Define sólo aquellas constantes que no están definidas. Sin lanzar excepciones. */
+			defined( $key ) || define( $key, $value );
+		}
+  }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,105 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'dealerdirect/phpcodesniffer-composer-installer' => 
+    array (
+      'pretty_version' => 'v0.7.2',
+      'version' => '0.7.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1c968e542d8843d7cd71de3c5c9c3ff3ad71a1db',
+    ),
+    'graham-campbell/result-type' => 
+    array (
+      'pretty_version' => 'v1.0.4',
+      'version' => '1.0.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0690bde05318336c7221785f2a932467f98b64ca',
+    ),
+    'phpoption/phpoption' => 
+    array (
+      'pretty_version' => '1.8.1',
+      'version' => '1.8.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'eab7a0df01fe2344d172bff4cd6dbd3f8b84ad15',
+    ),
+    'squizlabs/php_codesniffer' => 
+    array (
+      'pretty_version' => '3.6.2',
+      'version' => '3.6.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5e4e71592f69da17871dba6e80dd51bce74a351a',
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'v1.25.0',
+      'version' => '1.25.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '30885182c981ab175d4d034db0f6f469898070ab',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.25.0',
+      'version' => '1.25.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0abb51d2f102e00a4eefcf46ba7fec406d245825',
+    ),
+    'symfony/polyfill-php80' => 
+    array (
+      'pretty_version' => 'v1.25.0',
+      'version' => '1.25.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4407588e0d3f1f52efb65fbe92babe41f37fe50c',
+    ),
+    'vlucas/phpdotenv' => 
+    array (
+      'pretty_version' => 'v5.4.1',
+      'version' => '5.4.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '264dce589e7ce37a7ba99cb901eed8249fbec92f',
+    ),
+    'wp-coding-standards/wpcs' => 
+    array (
+      'pretty_version' => '2.3.0',
+      'version' => '2.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7da1894633f168fe244afc6de00d141f27517b62',
+    ),
+  ),
+);
